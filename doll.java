acc68221daf0c8a78ebe59906d9aa2b654ab0ded
valentineetrd <akinfeayomide6@gmail.com>
2025-09-09 15:17:06 +0100

    just added this

---
+++ doll.java
@@ -0,0 +1,15 @@
+!DOCTYPE html>
+<html>
+<head>
+    <title>My Doll Collection</title>
+</head>
+<body>
+    <h1>Welcome to My Doll Collection</h1>
+    <p>Here are some of my favorite dolls:</p>
+    <ul>
+        <li>Barbie</li>
+        <li>American Girl</li>
+        <li>Bratz</li>
+    </ul>
+</body>
+</html>
